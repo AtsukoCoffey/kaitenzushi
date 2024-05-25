fbc12402efefea14a9fc7daaf496623a01890b06
Atsuko Coffey <atsucoffey@gmail.com>
2024-05-25 14:37:52 +0100

    fix: Function validateInput - change innerText to value

--- assets/js/script.js
+++ assets/js/script.js
@@ -242,7 +242,7 @@ function handleKeyPress(event) {
   *///   // Enter key action
 function validateInput(event) {
 
-  if (textOver.innerText == textDisplay.innerText || input.innerText == textDisplay.innerText) {
+  if (textOver.innerText == textDisplay.innerText || input.value == textDisplay.innerText) {
 
     // letter counter reset
     letterCounter = 0;
