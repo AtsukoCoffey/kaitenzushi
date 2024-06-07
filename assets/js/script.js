7b1b28cbc48d745603058878bc5b77fb5a168228
Atsuko Coffey <atsucoffey@gmail.com>
2024-06-07 15:41:52 +0100

    feat: Add English translation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,175 +4,204 @@ const words = [
     name: 'amaebi',
     "kana": 'あまえび',
     image: 'assets/images/sushi_amaebi.webp',
-    sound: 'assets/sounds/su-amaebi.mp3'
+    sound: 'assets/sounds/su-amaebi.mp3',
+    english: 'Sweet shrimp'
   },
   {
     name: 'anago',
     "kana": 'あなご',
     image: 'assets/images/sushi_anago.webp',
-    sound: 'assets/sounds/su-anago.mp3'
+    sound: 'assets/sounds/su-anago.mp3',
+    english: 'Conger eel'
   },
   {
     name: 'battera',
     "kana": 'ばってら',
     image: 'assets/images/sushi_battera.webp',
-    sound: 'assets/sounds/su-battera.mp3'
+    sound: 'assets/sounds/su-battera.mp3',
+    english: 'Mackerel sushi'
   },
   {
     name: 'sabazushi',
     "kana": 'さばずし',
     image: 'assets/images/sushi_bouzushi_saba.webp',
-    sound: 'assets/sounds/su-sabazushi.mp3'
+    sound: 'assets/sounds/su-sabazushi.mp3',
+    english: 'Mackerel sushi'
   },
   {
     name: 'buri',
     "kana": 'ぶり',
     image: 'assets/images/sushi_buri.webp',
-    sound: 'assets/sounds/su-buri.mp3'
+    sound: 'assets/sounds/su-buri.mp3',
+    english: 'Big yellowtail'
   },
   {
     name: 'chirashizushi',
     "kana": 'ちらしずし',
     image: 'assets/images/sushi_chirashizushi.webp',
-    sound: 'assets/sounds/su-chirashizushi.mp3'
+    sound: 'assets/sounds/su-chirashizushi.mp3',
+    english: 'Sprinkled sushi'
   },
   {
     name: 'chutoro',
     "kana": 'ちゅうとろ',
     image: 'assets/images/sushi_chutoro.webp',
-    sound: 'assets/sounds/su-chutoro.mp3'
+    sound: 'assets/sounds/su-chutoro.mp3',
+    english: 'Medium tuna Toro'
   },
   {
     name: 'ebi',
     "kana": 'えび',
     image: 'assets/images/sushi_ebi.webp',
-    sound: 'assets/sounds/su-ebi.mp3'
+    sound: 'assets/sounds/su-ebi.mp3',
+    english: 'Shrimp'
   },
   {
     name: 'hamachi',
     "kana": 'はまち',
     image: 'assets/images/sushi_hamachi.webp',
-    sound: 'assets/sounds/su-hamachi.mp3'
+    sound: 'assets/sounds/su-hamachi.mp3',
+    english: 'Medium yellowtail'
   },
   {
     name: 'hanba-gu',
     "kana": 'はんばーぐ',
     image: 'assets/images/sushi_hamburg_tare.webp',
-    sound: 'assets/sounds/su-hanba-gu.mp3'
+    sound: 'assets/sounds/su-hanba-gu.mp3',
+    english: 'hamburg'
   },
   {
     name: 'harasu',
     "kana": 'はらす',
     image: 'assets/images/sushi_harasu.webp',
-    sound: 'assets/sounds/su-harasu.mp3'
+    sound: 'assets/sounds/su-harasu.mp3',
+    english: 'Salmon belly'
   },
   {
     name: 'hotate',
     "kana": 'ほたて',
     image: 'assets/images/sushi_hotate.webp',
-    sound: 'assets/sounds/su-hotate.mp3'
+    sound: 'assets/sounds/su-hotate.mp3',
+    english: 'Scallop'
   },
   {
     name: 'ika',
     "kana": 'いか',
     image: 'assets/images/sushi_ika.webp',
-    sound: 'assets/sounds/su-ika.mp3'
+    sound: 'assets/sounds/su-ika.mp3',
+    english: 'Squid'
   },
   {
     name: 'ikura',
     "kana": 'いくら',
     image: 'assets/images/sushi_ikura.webp',
-    sound: 'assets/sounds/su-ikura.mp3'
+    sound: 'assets/sounds/su-ikura.mp3',
+    english: 'Salmon roe'
   },
   {
     name: 'inarizushi',
     "kana": 'いなりずし',
     image: 'assets/images/sushi_inarizushi_set.webp',
-    sound: 'assets/sounds/su-inarizushi.mp3'
+    sound: 'assets/sounds/su-inarizushi.mp3',
+    english: 'Pickled fried tofu and sushi'
   },
   {
     name: 'hokkigai',
     "kana": 'ほっきがい',
     image: 'assets/images/sushi_kai_hokkigai.webp',
-    sound: 'assets/sounds/su-hokkigai.mp3'
+    sound: 'assets/sounds/su-hokkigai.mp3',
+    english: 'Hokkaido clam'
   },
   {
     name: 'katsuo',
     "kana": 'かつお',
     image: 'assets/images/sushi_katsuo.webp',
-    sound: 'assets/sounds/su-katsuo.mp3'
+    sound: 'assets/sounds/su-katsuo.mp3',
+    english: 'Bonito'
   },
   {
     name: 'kazunoko',
     "kana": 'かずのこ',
     image: 'assets/images/sushi_kazunoko.webp',
-    sound: 'assets/sounds/su-kazunoko.mp3'
+    sound: 'assets/sounds/su-kazunoko.mp3',
+    english: 'Herring roe'
   },
   {
     name: 'kappa',
     "kana": 'かっぱ',
     image: 'assets/images/sushi_makimono_kappa.webp',
-    sound: 'assets/sounds/su-kappa.mp3'
+    sound: 'assets/sounds/su-kappa.mp3',
+    english: 'Cucumber'
   },
   {
     name: 'nattou',
     "kana": 'なっとう',
     image: 'assets/images/sushi_makimono_nattou.webp',
-    sound: 'assets/sounds/su-nattou.mp3'
+    sound: 'assets/sounds/su-nattou.mp3',
+    english: 'Natto'
   },
   {
     name: 'negitoro',
     "kana": 'ねぎとろ',
     image: 'assets/images/sushi_makimono_negitoro.webp',
-    sound: 'assets/sounds/su-negitoro.mp3'
+    sound: 'assets/sounds/su-negitoro.mp3',
+    english: 'Tuna & Spring onion'
   },
   {
     name: 'takuan',
     "kana": 'たくあん',
     image: 'assets/images/sushi_makimono_takuwan.webp',
-    sound: 'assets/sounds/su-takuan.mp3'
+    sound: 'assets/sounds/su-takuan.mp3',
+    english: 'Pickled radish'
   },
   {
     name: 'torotaku',
     "kana": 'とろたく',
     image: 'assets/images/sushi_makimono_torotaku.webp',
-    sound: 'assets/sounds/su-torotaku.mp3'
+    sound: 'assets/sounds/su-torotaku.mp3',
+    english: 'Tuna & Pickled radish'
   },
   {
     name: 'sake',
     "kana": 'さけ',
     image: 'assets/images/sushi_salmon.webp',
-    sound: 'assets/sounds/su-sake.mp3'
+    sound: 'assets/sounds/su-sake.mp3',
+    english: 'Salmon'
   },
   {
     name: 'tai',
     "kana": 'たい',
     image: 'assets/images/sushi_tai.webp',
-    sound: 'assets/sounds/su-tai.mp3'
+    sound: 'assets/sounds/su-tai.mp3',
+    english: 'Sea bream'
   },
   {
     name: 'tako',
-    "kana": 'たこ',
+    kana: 'たこ',
     image: 'assets/images/sushi_tako.webp',
-    sound: 'assets/sounds/su-tako.mp3'
+    sound: 'assets/sounds/su-tako.mp3',
+    english: 'Octopus'
   },
   {
     name: 'tamago',
-    "kana": 'たまご',
+    kana: 'たまご',
     image: 'assets/images/sushi_tamago.webp',
-    sound: 'assets/sounds/su-tamago.mp3'
+    sound: 'assets/sounds/su-tamago.mp3',
+    english: 'Egg'
   },
   {
     name: 'temakizushi',
     "kana": 'てまきずし',
     image: 'assets/images/sushi_temakizushi.webp',
-    sound: 'assets/sounds/su-temakizushi.mp3'
+    sound: 'assets/sounds/su-temakizushi.mp3',
+    english: 'Hand-rolled sushi'
   },
   {
     name: 'uni',
     "kana": 'うに',
     image: 'assets/images/sushi_uni.webp',
-    sound: 'assets/sounds/su-uni.mp3'
+    sound: 'assets/sounds/su-uni.mp3',
+    english: 'Sea urchin'
   },
 ];
 
@@ -292,6 +321,7 @@ const textDisplay = document.getElementById('text-display');
 const textOver = document.getElementById('text-overlay');
 const kanaDisplay = document.getElementById('kana-display');
 const kanaOver = document.getElementById('kana-overlay');
+const english = document.getElementById('english');
 const input = document.getElementById('input'); // input box
 
 // Game counter  
@@ -344,20 +374,15 @@ function loadGame() {
   // Set interval and after 60 seconds finishGame()
   let timeDisplay = 60;
   document.getElementById('time-left').textContent = `${timeDisplay}`;
-
-    const globalTimerId = setInterval(() => {
-      timeDisplay--;
-      document.getElementById('time-left').textContent = `${timeDisplay}`;
-      if (timeDisplay === 0) {
-        clearInterval(globalTimerId);
-        finishGame();
-      }
-    }, 1000)
-  // // Set global timer (60s)
-  // setTimeout(function () {
-  //   finishGame();
-  //   // Time limit 60 secounds
-  // }, 60000);
+  // Display time limit every second
+  const globalTimerId = setInterval(() => {
+    timeDisplay--;
+    document.getElementById('time-left').textContent = `${timeDisplay}`;
+    if (timeDisplay === 0) {
+      clearInterval(globalTimerId);
+      finishGame();
+    }
+  }, 1000)
   // Focus on input area
   input.focus();
   // All the counter reset
@@ -394,6 +419,7 @@ function nextWord() {
   imageDisplay.innerHTML = '<img src="' + shuffledWords[gameCounter].image + '" width="100" height="100" alt="Question word">';
   kanaDisplay.innerHTML = shuffledWords[gameCounter].kana;
   textDisplay.innerHTML = shuffledWords[gameCounter].name;
+  english.innerHTML = shuffledWords[gameCounter].english;
 
   // Play word sound
   wordAudio.setAttribute('src', shuffledWords[gameCounter].sound);
